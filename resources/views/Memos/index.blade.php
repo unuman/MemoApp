2cfc909ef44f552093e2c22f9202b9a779bb47ac
unuman <unumakyohey@gmail.com>
2023-10-29 15:23:53 +0900

    s

--- resources/views/Memos/index.blade.php
+++ resources/views/Memos/index.blade.php
@@ -1,11 +1,21 @@
-<h1>出品メモ一覧</h1>
-<ul>
-    <!-- リンク先をidで取得し名前で出力 -->
-    @foreach ($memos as $memo)
-        <li><a href="{{ route('memos.show', $memo) }}">{{ $memo->title }}</a></li>
-    @endforeach
-
-</ul>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>memo index</title>
+</head>
+<body>
+    <h1>タイトル一覧</h1>
+    <ul>
+        @foreach ($memos as $memo)
+            <!-- // リンク先をidで取得し名前で出力 -->
+            <li><a href="{{ route('memos.show', $memo) }}">{{ $memo->title }}</a></li>
+        @endforeach
+    </ul>
 
     <!-- 新規登録画面へジャンプする -->
-    <button onclick='location.href="{{ route("memos.create") }}"'>登録する</button>
+    <button onclick="location.href='{{ route('memos.create') }}'">登録する</button>
+</body>
+</html> 
